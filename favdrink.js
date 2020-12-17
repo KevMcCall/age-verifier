75cbb03119e6954127994caf013e2b0613d01747
kevmccall <kev_mccall@yahoo.com>
2020-12-16 22:14:02 -0700

    added comments to JS and html UL

--- favdrink.js
+++ favdrink.js
@@ -1,33 +1,36 @@
 function favDrinks() {
     var text;
     var drinks = document.getElementById("inputbox").value;
-  
-    switch(drinks) {
-     case "Pineapple", "pineapple":
-      text = "Pineapple Rum Punch";
-      break;
-    case "Apple", "apple":
-      text = "Apple Whiskey Sour";
-      break;
-    case "Lime", "lime":
-      text = "Tequila Lime Mojito Cocktail";
-      break;
-    case "Cherry", "cherry":
-      text = "Cherry Lemonde Cocktail";
-      break;
-    case "Orange", "orange":
-        text = "Orange Crush";
-        break;
-    case "Cranberry", "cranberry":
-        text = "Cranberry Cosmopolitan";
-        break;
-    case "Banana", "Banana":
-        text = "Dirty Banana Cocktail";
-        break;
-    case "Watermelon", "watermelon":
-        text = "Watermelon Sangria";
-        break;
-    default: text = "There are no drinks with that selection."
+
+    //Switch(expression) to select code blocks to be executed.
+    switch (drinks) {
+        //Cases to be tested against the expression.
+        case "Pineapple", "pineapple":
+            text = "Pineapple Rum Punch";
+        case "Apple", "apple":
+            text = "Apple Whiskey Sour";
+            //If case matches, code will execute, and break will end switch block.
+            break;
+        case "Lime", "lime":
+            text = "Tequila Lime Mojito Cocktail";
+            break;
+        case "Cherry", "cherry":
+            text = "Cherry Lemonde Cocktail";
+            break;
+        case "Orange", "orange":
+            text = "Orange Crush";
+            break;
+        case "Cranberry", "cranberry":
+            text = "Cranberry Cosmopolitan";
+            break;
+        case "Banana", "Banana":
+            text = "Dirty Banana Cocktail";
+            break;
+        case "Watermelon", "watermelon":
+            text = "Watermelon Sangria";
+            break;
+        //If none of the cases match, default text is ran
+        default: text = "There are no drinks with that selection."
     }
     document.getElementById("favdrinkresult").innerHTML = text;
-  }
+}
